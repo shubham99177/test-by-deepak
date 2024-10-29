8fec873d71f8abda5de3cc8f2dbe096f011c6ddc
Shubham Rathore <119403207+shubham99177@users.noreply.github.com>
2024-10-29 22:08:09 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Adjust to your backend URL
+  baseURL: 'https://test-by-deepak-8.onrender.com/api', // Adjust to your backend URL
 });
 
 // Add request interceptor to include the token
